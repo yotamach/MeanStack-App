22724745696b5a701ee1604f7518d065180d4ada
Yotam Achrak <yotam2585@gmail.com>
2018-09-17 17:19:07 +0300

    Adding spinners

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
+
 const app = express();
 
 mongoose.connect('mongodb+srv://yot_yot:8Co0eM4xryUTGBqR@cluster0-hx5db.mongodb.net/MeanStack?retryWrites=true',{ useNewUrlParser: true })
@@ -22,42 +23,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then((createdPost) => {
-    console.log(post);
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-    });
-  }).catch((err) => {
-    console.log(err);
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.findOneAndRemove({_id: req.params.id}).then((result) => {
-    console.log(result);
-    res.status(200).json({message: "Post deleted!"});
-  });
-});
-
+app.use("/api/posts",postsRoutes);
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {
   MatCardModule,
   MatButtonModule,
   MatToolbarModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatProgressSpinnerModule
 } from "@angular/material";
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -25,12 +27,14 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatProgressSpinnerModule,
     MatExpansionModule,
     HttpClientModule
   ],
